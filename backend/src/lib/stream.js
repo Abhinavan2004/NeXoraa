c71b90c95c76e7cb742f3672ca4536c35987b88e
Abhinavan2004 <abhinavanpan@gmail.com>
2025-06-09 03:10:32 +0530

    Adde DaisyUI & React-toaster , Routes Page

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -4,12 +4,12 @@ require("dotenv").config({ path: "../.env" });
 const apikey = process.env.STREAM_API_SECRET_KEY ;
 const apikey_secret = process.env.STREAM_API_SECRET ;
 
-if(!apikey || !apikey_secret){
+if( !apikey || !apikey_secret){
     console.log("Missing API_key or API_key_Secret");
 }
 
 const StreamClient = StreamChat.getInstance(apikey, apikey_secret)
-
+ f
 const Upstreamer = async (userData) =>{
     try{
         await StreamClient.upsertUsers([userData]);  // Upsert helps in creting users in Stream if not present but if present then helps in Updating them ..
@@ -20,7 +20,6 @@ const Upstreamer = async (userData) =>{
         throw err;  // Throwing error to be handled in the calling function
     }
 }
-
 
 
 const generateStreamToken = (userId) => {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage.jsx';
+import CallPage from './pages/CallPage.jsx';
+import ChatPage from './pages/ChatPage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import SignupPage from './pages/SignupPage.jsx';
+import OnBoardPage from './pages/OnboardPAge.jsx';
+import NotificationsPage from './pages/NotificationsPage.jsx';
+import toast, { Toaster } from 'react-hot-toast';
+
 
 const App = () => {
-  return (
-    <div className='text-5xl'>App
-<button class="btn btn-primary">One</button>
-<button class="btn btn-secondary">Two</button>
-<button class="btn btn-accent btn-outline">Three</button>
-</div>
-  )
-}
+  const notify = () => toast('Here is your toast.');
 
-export default App
+  return (
+    <div className="text-5xl h-screen" data-theme="valentine">
+      App
+      
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/call" element={<CallPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/onboard" element={<OnBoardPage />} />
+        <Route path="/notifications" element={<NotificationsPage />} />
+
+    <div>
+      <button onClick={notify}>Make me a toast</button>
+      <Toaster />
+    </div>
+        <Toaster />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
