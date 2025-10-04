b7ea761c578d5a373562cf541ac76d6412807d4c
Abhinav Anpan <143207723+Abhinavan2004@users.noreply.github.com>
2025-10-05 01:53:49 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.mode === 'development'? "http://localhost:4000/api" : "/api"; // Adjust the base URL based on the environment
+const BASE_URL = import.meta.env.mode === 'development'? "http://localhost:4000/api" : "https://nexoraa-backend.onrender.com/api"; // Adjust the base URL based on the environment
 const axiosInstance = axios.create({
-    // baseURL:"/api",                       // Use relative path for local development
     baseURL: BASE_URL,
     withCredentials: true, // This allows cookies to be sent with requests
 
